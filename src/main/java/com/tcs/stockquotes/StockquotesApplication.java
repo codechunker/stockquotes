cd504138ce7e139bb8b7cf40c1d3c30a7d05d9a9
Remy Ohajinwa <remy.ohajinwa@interswitchgroup.com>
2021-07-29 22:27:52 +0100

    Initial Commit

---
+++ src/main/java/com/tcs/stockquotes/StockquotesApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.stockquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockquotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockquotesApplication.class, args);
+	}
+
+}
