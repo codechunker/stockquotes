5be4f217bc4a69bb56257cb9cc5f097098246265
Remy Ohajinwa <remy.ohajinwa@interswitchgroup.com>
2021-07-30 08:31:24 +0100

    application

--- src/test/java/com/tcs/stockquotes/StockquotesApplicationTests.java
+++ src/test/java/com/tcs/stockquotes/StockquotesApplicationTests.java
@@ -1,13 +1,12 @@
 package com.tcs.stockquotes;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StockquotesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
