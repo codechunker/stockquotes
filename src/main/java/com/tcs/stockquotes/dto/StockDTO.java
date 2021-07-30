ec9f799e7d65304dada8c590503efe4292466045
Remy Ohajinwa <remy.ohajinwa@interswitchgroup.com>
2021-07-30 09:27:48 +0100

    added lombok

--- src/main/java/com/tcs/stockquotes/dto/StockDTO.java
+++ src/main/java/com/tcs/stockquotes/dto/StockDTO.java
@@ -1,19 +1,14 @@
 package com.tcs.stockquotes.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StockDTO implements Serializable {
-
     private String name;
-
-    public StockDTO(String name) {
-        this.name = name;
-    }
-
-    public StockDTO() {
-    }
-
-    public String getName() {
-        return name;
-    }
 }
--- src/main/java/com/tcs/stockquotes/dto/StockResponse.java
+++ src/main/java/com/tcs/stockquotes/dto/StockResponse.java
@@ -1,18 +1,12 @@
 package com.tcs.stockquotes.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockResponse {
-
     private String content;
-
-    public StockResponse(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
